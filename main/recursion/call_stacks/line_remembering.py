0f5d19c77855501fc0d4219c4a49d550115f5db2
minte9 <catalinprescure@gmail.com>
2023-03-20 21:10:06 +0200

    algorithms-pages update

--- main/recursion/call_stacks/line_remembering.py
+++ main/recursion/call_stacks/line_remembering.py
@@ -4,21 +4,21 @@ finishises its execution.
 """
 
 def a():
-    print('a() Line 07'); b()
-    print('a() Line 08')
+    print('a - Line 07'); b()
+    print('a - Line 08')
 def b():
-    print(' b() Line 11'); c()
-    print(' b() Line 12')
+    print('b - Line 11'); c()
+    print('b - Line 12')
 def c():
-    print('  c() Line 13')
-    print('  c() Line 14')
+    print('c - Line 13')
+    print('c - Line 14')
 a()
 
 """
-a() Line 07
-b() Line 11
-c() Line 13
-c() Line 14
-b() Line 12
-a() Line 08
+a - Line 07
+b - Line 11
+c - Line 13
+c - Line 14
+b - Line 12
+a - Line 08
 """
