810cbe06a2d9f62817e8c4dbd81fef04c6d03b38
minte9 <catalinprescure@gmail.com>
2023-03-22 09:48:29 +0200

    algorithms-pages update

--- main/recursion/base_case/base_case.py
+++ main/recursion/base_case/base_case.py
@@ -6,21 +6,21 @@ at least one recursive case.
 
 def a(repeat, i=0):
     i = i  + 1
-    print('Frame', i, 'a(repeat=%s) called' % repeat)
+    print('Frame', i, '- repeat = ' + str(repeat))
 
-    if not repeat: # Base case
-        print('Frame', i, 'Base case')
+    if not repeat:
+        print('Frame', i, '- return / Base case')
         return
 
-    a(False, i) # Recursive case
-    print('Frame', i, 'Recursive case')
+    a(False, i)
+    print('Frame', i, '- return / Recursive case')
     return
     
 a(True)
 
 """
-    Frame 1 a(repeat=True) called
-    Frame 2 a(repeat=False) called
-    Frame 2 Base case
-    Frame 1 Recursive case
+    Frame 1 - repeat = True
+    Frame 2 - repeat = False
+    Frame 2 - return / Base case
+    Frame 1 - return / Recursive case
 """
