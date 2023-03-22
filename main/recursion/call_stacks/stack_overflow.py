ad3c9e0dc35ae8074339cae48b87cf09a2f173c6
minte9 <catalinprescure@gmail.com>
2023-03-22 09:34:30 +0200

    algorithms-pages update

--- main/recursion/call_stacks/stack_overflow.py
+++ main/recursion/call_stacks/stack_overflow.py
@@ -1,11 +1,7 @@
 """ Stack overflow
-A recurson function is a function that calls itself.
-
 The limit of calls is called maximum recursion depth.
+Stack overflows don't damage the computer, it just terminatet the program.
 For Python, this size is set to 1000.
-
-Stack overflows don't damage the computer, 
-it just terminatet the program.
 """
 
 def repeat():
