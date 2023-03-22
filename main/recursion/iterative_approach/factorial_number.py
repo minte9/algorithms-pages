6a955f2d625e08acb4735ae983cfb1f2c03b091e
minte9 <catalinprescure@gmail.com>
2023-03-22 10:32:48 +0200

    algorithms-pages update

--- main/recursion/iterative_approach/factorial_number.py
+++ main/recursion/iterative_approach/factorial_number.py
@@ -1,26 +1,22 @@
-""" Factorial
+""" Factorial 4! = 4x3x2x1
 Many computer science courses use factorial calculation 
 as a classic example of a recursive function.
-
 Factorials are use for example in finding permutations number.
-    4! = 4x3x2x1
+    
 Iterrative approch, multiplies intergers 1 to n in a loop
 Recursive approach, uses neighbours 5! = 5 * 4!
+
 The recursive implementation requires 5 frame objects
 The iterrative implementation, only 1.
-
-For calculating factorials the iterative approach is better!
 """
 
-# Iterrative
-def ifactorial(n):
+def ifactorial(n): # Iterrative
     p = 1
     for i in range(1, n+1):
         p = p * i
     return p
 
-# Recursive 
-def rfactorial(n):
+def rfactorial(n): # Recursive 
     if n == 1:
         return 1
     return n * rfactorial(n-1)
@@ -34,9 +30,7 @@ assert rfactorial(4) == 4 * rfactorial(3)
 # Limits
 n = ifactorial(1001)
 print(len(str(n))) # 2571
-
 try:    
     n = rfactorial(1001)
 except RecursionError as e:
     print(e) # maximum recursion depth exceeded in comparison
-
