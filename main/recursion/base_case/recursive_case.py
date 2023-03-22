4b6c95d631cbcd9e14a8f7761be4f637866701c4
minte9 <catalinprescure@gmail.com>
2023-03-22 09:55:53 +0200

    algorithms-pages update

--- main/recursion/base_case/recursive_case.py
+++ main/recursion/base_case/recursive_case.py
@@ -1,20 +1,16 @@
 """ Recursive case
-Returning from base case doesn't immediately return
-from all the recursive calls that happend before it.
-
 The code in a recursive call can be split in two partes:
 the code before and the code after.
 """
 
 def a(n):
     print(n)
+
     if n == 0:
-        print('Base case')
-        return
+        print('Base case'); return
 
     a(n-1)
-    print(n, 'returning')
-    return
+    print(n, 'returning'); return
 
 a(3)
 
