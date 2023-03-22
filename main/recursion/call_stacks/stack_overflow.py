dbe5154c60938ffd2962c491852f7bc01ace7b75
minte9 <catalinprescure@gmail.com>
2023-03-22 09:35:13 +0200

    algorithms-pages update

--- main/recursion/call_stacks/stack_overflow.py
+++ main/recursion/call_stacks/stack_overflow.py
@@ -1,11 +1,10 @@
 """ Stack overflow
 The limit of calls is called maximum recursion depth.
-Stack overflows don't damage the computer, it just terminatet the program.
+Stack overflows don't damage the computer, it just terminate the program.
 For Python, this size is set to 1000.
 """
 
 def repeat():
     repeat()
 
-repeat() 
-    # RecursionError: maximum recursion depth exceeded
+repeat() # RecursionError: maximum recursion depth exceeded
