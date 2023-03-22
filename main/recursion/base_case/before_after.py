d503d3c287fc4516632bdeb4839f0eb865731d4d
minte9 <catalinprescure@gmail.com>
2023-03-22 10:28:09 +0200

    algorithms-pages update

--- main/recursion/base_case/before_after.py
+++ main/recursion/base_case/before_after.py
@@ -5,24 +5,24 @@ of the recursive algorithm.
 """
 
 def a(n):
-    print(n, '- Before')
-    
+    print('Before', '\t', 'n =', n)
+
     if n == 0:
-        print('Base case')
+        print('Base', '\t', 'n =', n)
         return
 
     a(n-1) # recursive call
 
-    print(n, '- After')
+    print('After', '\t', 'n =', n)
     return
 
 a(2)
 
 """
-    2
-    1
-    0
-    Base case
-    1 returning
-    2 returning
+    Before   n = 2
+    Before   n = 1
+    Before   n = 0
+    Base     n = 0
+    After    n = 1
+    After    n = 2
 """
