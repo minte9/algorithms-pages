07c4f0562719f05ed13dcb5c699a77355f9e9f39
minte9 <catalinprescure@gmail.com>
2023-04-05 16:03:38 +0300

    algorithms-pages update

--- main/minimax/tic_tac_toe/TTT3_game.py
+++ main/minimax/tic_tac_toe/TTT3_game.py
@@ -2,6 +2,7 @@
 """
 
 import numpy as np
+import random, sys
 
 def get_legal_moves(board):
     moves = []
@@ -37,8 +38,8 @@ def show(board, move=None):
         CYAN, ENDC = '\033[96m', '\033[0m' # colored move
         board_[i][j] = CYAN + board[move] + ENDC 
 
-    if 1 == 0: 
-        GRAY, ENDC = '\033[90m', '\033[0m' # color numbers
+    if 1 == 1: # activate colored numbers
+        GRAY, ENDC = '\033[90m', '\033[0m'
         k = 0
         for i in range(3):
             for j in range(3):
@@ -50,6 +51,8 @@ def show(board, move=None):
         print(" ", board_[i][0], "|", board_[i][1], "|", board_[i][2])
         print(" ---+---+---") if i < 2 else ""
 
+
+# Minimax algorithm
 
 def minimax(board, player=True, alpha=float('-inf'), beta=float('inf')):
     
@@ -85,24 +88,63 @@ def minimax(board, player=True, alpha=float('-inf'), beta=float('inf')):
     return best_move, best_score
 
 
+# Game
+
 def human_move(board):
     while True:
         n = input("\nEnter your move (1-9): ")
+        
+        if n.upper() == 'Q':
+            sys.exit()
+
         if not n.isdigit(): 
             continue
+        
         y = (int(n) - 1) // 3
         x = (int(n) - 1)  % 3
         if (y, x) in get_legal_moves(board): 
             return y, x
 
+def start_board():
+    return np.array([
+            [" ", " ", " "], 
+            [" ", " ", " "],
+            [" ", " ", " "],
+        ])
 
-def game(board, player=True, expected=None):
-
+def game(board, player=True, total_score=0, X_score=0):    
+    
     if player == True:
         move = human_move(board)
-    else:
+    
+    if player == False:
         print("\nComputer move:")
         move, score = minimax(board, player)
+        moves = get_legal_moves(board)
+
+        # Variation: make the first computer move random (more interesting)
+        if 1 == 1:
+            if len(moves) == 8:
+                move = random.choice(moves)
+
+            # In this variation we need to prioritize the move that ends the game 
+            # on next move (because we'll have multiple 'X' win moves)
+            end = False
+            for m in moves:
+                new_board = np.copy(board); 
+                new_board[m] = 'O'
+                if is_terminal_state(new_board): 
+                    move = m
+                    end = True
+                    break
+
+            if end == False: # don't check for X if we already found an O end
+                for m in moves:
+                    new_board = np.copy(board); 
+                    new_board[m] = 'X'
+                    if is_terminal_state(new_board): 
+                        move = m
+                        break
 
     board[move] = 'X' if player else 'O'
     show(board, move)
@@ -112,18 +154,27 @@ def game(board, player=True, expected=None):
         if score ==  1: print('\nX won!\n')
         if score == -1: print('\nO won!\n')
         if score ==  0: print('\nDraw!\n')
-        return
 
-    game(board, not player) # Recursive case
+        if score == 1: 
+            total_score += 1
+            X_score     += 1 
+        if score == 0: 
+            total_score += 1
+            X_score     += 0.5
+        if score == -1: 
+            total_score += 1
+            X_score     += 0
+
+        print("Your score: " + str(int(X_score * 100 / total_score)) + "% " + "\n")
+        board = start_board() # reset
+        show(board)
+        player = False
+                
+    game(board, not player, total_score, X_score) # Recursive case
 
 
-# Start board
-board = np.array([
-    [" ", " ", " "], 
-    [" ", " ", " "],
-    [" ", " ", " "],
-])
 
-print("\nTic Tac Toe\n")
+print("\nTic Tac Toe (computer first response is random, Q to exit)\n")
+board = start_board()
 show(board)
 game(board, True)
