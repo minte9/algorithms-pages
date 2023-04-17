cf16e3e320ba26a7c60f93c532efd6ce6bc3690c
minte9 <catalinprescure@gmail.com>
2023-04-17 16:50:40 +0300

    algorithms-pages update

--- main/divide_conquer/quicksort/qs_recursive.py
+++ main/divide_conquer/quicksort/qs_recursive.py
@@ -12,20 +12,17 @@ one book (the base case). And the piles are now sorted.
 """
 
 def quicksort(items, i=0, j=None):
-    
     if j == None: j = len(items) - 1
-
     if i > j: 
         return # stop sorting (base case)
-    
+
     pivot = items[j]
 
     for k in range(i, j + 1):
-
+        
         if items[k] < pivot:
             items[i], items[k] = items[k], items[i] # swap items
             i = i + 1
-
         if items[k] == pivot:
             items[i], items[k] = items[k], items[i] # move pivot
             
