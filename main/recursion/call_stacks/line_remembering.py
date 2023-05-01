75254fc84090c549945ea98dff231e6a0158053d
minte9 <catalinprescure@gmail.com>
2023-05-01 16:21:18 +0300

    algorithms-pages update

--- main/recursion/call_stacks/line_remembering.py
+++ main/recursion/call_stacks/line_remembering.py
@@ -7,17 +7,17 @@ def line_remembering_function():
     print('Line 08')
     goto()
     
-    print('Line 10 - return to first function')
+    print('Line 10')
     return
 
 def goto():
-    print('Line 14')
+    print('Line 14 - return to first function')
     return
 
 line_remembering_function()
 
 """
     Line 08
-    Line 14
-    Line 10 - return to first function
+    Line 14 - return to first function
+    Line 10
 """
