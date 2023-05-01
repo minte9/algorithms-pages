f3e996b91167f37226e927a955380f80f186423a
minte9 <catalinprescure@gmail.com>
2023-05-01 21:20:16 +0300

    algorithms-pages update

--- main/recursion/iterative_approach/factorial_number.py
+++ main/recursion/iterative_approach/factorial_number.py
@@ -9,7 +9,7 @@ Iterrative, multiplies intergers 1 to n in a loop,
 
 """
 
-def factorial(n): # Iterative
+def factorial_iterative(n):
     p = 1
     for i in range(1, n+1):
         p = p * i
@@ -21,13 +21,13 @@ def factorial_recursive(n):
     return n * factorial_recursive(n-1)
 
 # Tests
-assert factorial(4) == 24
-assert factorial(5) == 120
+assert factorial_iterative(4) == 24
+assert factorial_iterative(5) == 120
 assert factorial_recursive(5) == 5 * factorial_recursive(4)
 assert factorial_recursive(4) == 4 * factorial_recursive(3)
 
 # Limits
-n = factorial(1001)
+n = factorial_iterative(1001)
 print(len(str(n))) # 2571
 try:    
     n = factorial_recursive(1001)
