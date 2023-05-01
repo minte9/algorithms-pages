2f889a8eb4c9ade1c03266b66be2960199dd13fa
minte9 <catalinprescure@gmail.com>
2023-05-01 21:23:12 +0300

    algorithms-pages update

--- main/recursion/iterative_approach/fibonacci_sequence.py
+++ main/recursion/iterative_approach/fibonacci_sequence.py
@@ -12,9 +12,7 @@ It repeats same calculation over and over.
 For fibonacci(5) the fibonacci(2) is called four times!
 """
 
-import time
-
-def fibonacci(n): # Iterative
+def fibonacci_iterative(n):
     a, b = 1, 1
     for i in range(1, n-1):
         nextb = a + b
@@ -28,24 +26,22 @@ def fibonacci_recursive(n):
     if n == 2: return 1
     return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
 
-assert fibonacci(2) == 1
-assert fibonacci(3) == 2
-assert fibonacci(4) == 3
-assert fibonacci(5) == 5
+# Tests
+assert fibonacci_iterative(2) == 1
+assert fibonacci_iterative(3) == 2
+assert fibonacci_iterative(4) == 3
+assert fibonacci_iterative(5) == 5
 assert fibonacci_recursive(6) == 8
 assert fibonacci_recursive(7) == 13
 
-start = time.time()
-n = fibonacci(100)
-print("Iterative: fibonacci(100) =", n, time.time() - start, 'seconds') 
-
-start = time.time()
-n = fibonacci_recursive(36)
-print("Recursive: fibonacci(36)  =", n, "\t\t", time.time() - start, 'seconds') 
+# Time
+import time
+t1 = time.time(); n1 = fibonacci_iterative(100)
+t2 = time.time(); n2 = fibonacci_recursive(36)
+print("Iterative: fibonacci(100)", time.time() - t1, 's') 
+print("Recursive: fibonacci(36) ", time.time() - t2, 's') 
 
 """
-
-Iterative: fibonacci(100) = 354224848179261915075 7.152557373046875e-06 seconds
-Recursive: fibonacci(36)  = 14930352             2.724464178085327 seconds
-
+    Iterative: fibonacci(100) 2.5802948474884033 s
+    Recursive: fibonacci(36)  2.5803282260894775 s
 """
