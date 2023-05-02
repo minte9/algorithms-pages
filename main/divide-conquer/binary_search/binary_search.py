0d6e702f5662b42917d3cb976b476ce66c42209d
minte9 <catalinprescure@gmail.com>
2023-05-02 18:13:21 +0300

    algorithms-pages update

--- main/divide-conquer/binary_search/binary_search.py
+++ main/divide-conquer/binary_search/binary_search.py
@@ -5,7 +5,8 @@ A linear search of a shelf with 100 books takes 100 steps.
 A binary search on 50 takes 6 and on 100 only 7 steps.
 """
 
-import sys; sys.dont_write_bytecode = True # import custom module
+import sys
+sys.dont_write_bytecode = True # import custom module
 from decorators import timer
 
 @timer 
