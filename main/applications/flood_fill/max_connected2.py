9ac29d3c4e9e6f04b0cbb3f6b7fcbfa247bde960
minte9 <catalinprescure@gmail.com>
2023-05-20 18:47:01 +0300

    algorithms-pages update

--- main/applications/flood_fill/max_connected2.py
+++ main/applications/flood_fill/max_connected2.py
@@ -19,11 +19,13 @@ for y in range(len(grid)):
             
         # Right
         if x+1 < len(grid[0]):
-            if v == grid[y][x+1]: connections[str(v)] += 1
+            if v == grid[y][x+1]: 
+                connections[str(v)] += 1
 
         # Down
         if y+1 < len(grid):
-            if v == grid[y+1][x]: connections[str(v)] += 1
+            if v == grid[y+1][x]: 
+                connections[str(v)] += 1
 
 print(connections) 
     # {'0': 3, '1': 5, '2': 1}
