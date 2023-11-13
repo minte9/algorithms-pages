6db611d6b8383adcea6f75764845a574a5876937
minte9 <catalinprescure@gmail.com>
2023-11-13 09:28:54 +0200

    algorithms-pages update

---
+++ main/comparision-based/bubble_sort/bubble_sort.py
@@ -0,0 +1,15 @@
+def bubble_sort(X):
+    for i in range(len(X)):
+        for j in range(len(X) -i-1):
+            if X[j] > X[j+1]:
+                X[j], X[j+1] = X[j+1], X[j]
+
+# Example usage
+A = [1, 3, 9, 5, 4, 0]
+
+bubble_sort(A)
+print("Sorted list =", A)
+
+"""
+    [0, 1, 3, 4, 5, 9]
+"""
